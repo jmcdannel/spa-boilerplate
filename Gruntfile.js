f4e0a6cebe9a30032592fa78ed0d274eaf1f2b61
Josh McDannel <jmcdannel@walmartlabs.com>
2014-04-09 20:05:02 -0600

    added tasks to grunt

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,7 +1,11 @@
 module.exports = function(grunt) {
 
+  "use strict";
+  require("matchdep").filterDev("grunt-*").forEach(grunt.loadNpmTasks);
+
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
+
     handlebars: {
       compile: {
         options: { 
@@ -14,10 +18,55 @@ module.exports = function(grunt) {
           'www/js/app/compiled-templates.js': ['www/templates/*.hbs']
         }
       }
+    },
+
+    requirejs: {
+      compile: {
+        options: {
+          appDir: 'www',
+          baseUrl: "js/app",
+          mainConfigFile: "www/js/app.js",
+          name: "../app", // assumes a production build using almond
+          out: "js/optimized.js"
+        }
+      }
+    },
+
+    cssmin: {
+        build: {
+            src: 'www/css/styles.css',
+            dest: 'www/css/styles.min.css'
+        }
+    },
+
+    sass: {
+        build: {
+            files: {
+                'www/css/styles.css': 'www/sass/styles.scss'
+            }
+        }
+    },
+
+    watch: {
+      css: {
+          files: ['www/sass/**/*.scss'],
+          tasks: ['sass']
+      },
+      html: {
+        files: ['www/templates/*.hbs'],
+        tasks: ['handlebars']
+      }
     }
+
   });
   
 
+  //grunt.loadNpmTasks('grunt-contrib-requirejs');
+  //grunt.loadNpmTasks('grunt-contrib-handlebars');
 
-  grunt.loadNpmTasks('grunt-contrib-handlebars');
+
+  grunt.registerTask('default', []);
+  grunt.registerTask('dev', ['handlebars', 'sass']);
+  grunt.registerTask('build', ['handlebars', 'sass', 'cssmin', 'requirejs']);
+
 };
